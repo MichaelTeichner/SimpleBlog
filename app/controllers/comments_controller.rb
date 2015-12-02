86f4108d02a5eda3ebf63426a42080f740f843ef
Michael Teichner <michael.r.teichner@gmail.com>
2015-12-02 22:47:53 +0200

    Added comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+	before_action :authenticate_user!
+
+	def create
+		@comment = Comment.create(comment_params)
+		@post = Post.find(params[:post_id])
+
+		@comment.user_id = current_user.id
+		@comment.post_id = @post.id
+
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
+end
